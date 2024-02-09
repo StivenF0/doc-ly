3aed4f58faef91194789866e69285661455cc19e
StivenF0 <stivenfonseca201@gmail.com>
2024-02-09 18:09:09 -0300

    Added new button to sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import CloseIcon from "./CloseIcon";
+
+interface Props {
+  children?: any;
+}
+
+export default ({ children }: Props) => {
+  const [sidebar, setSidebar] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => setSidebar(true), 1000);
+  // }, []);
+
+  return (
+    <div
+      className={`absolute top-0 left-0 w-full h-full z-10 bg-gray-800/60 transition-opacity duration-300 ${
+        sidebar ? "opacity-100" : "opacity-0"
+      }`}
+    >
+      <div
+        className={`bg-white absolute h-full z-20 top-0 w-80 transition-all duration-300 pt-4 pr-5 ${
+          sidebar ? "left-0" : "-left-80"
+        }`}
+      >
+        <div className="flex w-full justify-end items-center">
+          <button className="text-3xl">
+            <CloseIcon />
+          </button>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
